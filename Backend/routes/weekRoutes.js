715b16980fd9ccbee41d7774d9bb6f737f8337e9
Ahmed <ahmed.alvi61@gmail.com>
2023-10-03 13:01:17 +0500

    verify token

--- Backend/routes/weekRoutes.js
+++ Backend/routes/weekRoutes.js
@@ -5,9 +5,9 @@ const weekController = require('../controller/weekController')
 const verifyToken = require('../middleware/auth/jwt')
 
 
-week_Routes.post('/add-weeks',weekController.add_weeks)
-.get('/get-weeks',weekController.get_weeks_data)
-.patch('/edit-week/:id',weekController.edit_week)
-.delete('/delete-week/:id',weekController.delete_week)
-.put('/weeks/:userId/:weekId/items/:itemId',weekController.item_marked)
+week_Routes.post('/add-weeks',verifyToken,weekController.add_weeks)
+.get('/get-weeks',verifyToken,weekController.get_weeks_data)
+.patch('/edit-week/:id',verifyToken,weekController.edit_week)
+.delete('/delete-week/:id',verifyToken,weekController.delete_week)
+.put('/weeks/:userId/:weekId/items/:itemId',verifyToken,weekController.item_marked)
 module.exports = week_Routes;
